98f2cba5ed46ed5347003bb16befa1086ef4529d
Itzel <itzeljimenez.do@gmail.com>
2017-08-09 21:45:24 +0000

    configuración gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,18 @@ gulp.task('sass', function(){
     .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
     .pipe(minifyCSS())
     .pipe(gulp.dest('public/css'))
+});
 
+gulp.task("cargarINDEX",()=>{
+	gulp.src('./src/' + "**/*.html")
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('cargarJS', () =>{
+	gulp.src('./src/' + 'js/*.js')
+		.pipe(gulp.dest('public/js'));
 });
 
 
 
-gulp.task('default', ['sass' ]);
+gulp.task('default', ['sass', 'cargarINDEX', 'cargarJS' ]);
