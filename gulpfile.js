39558de32fd8cbd3505ca53b7afec50cdf0fdd79
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-08-09 15:58:59 -0500

    terminado de flujo

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
 
 gulp.task('sass', function(){
-  return gulp.src('scss/*.scss')
+  return gulp.src('./src/scss/*.scss')
     .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
     .pipe(minifyCSS())
     .pipe(gulp.dest('public/css'))
 
 });
+
+
 
 gulp.task('default', ['sass' ]);
