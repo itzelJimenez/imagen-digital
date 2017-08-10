0b8c92c5badaf8c41f9d147508499f2694fcdb3d
Itzel <itzeljimenez.do@gmail.com>
2017-08-10 13:01:07 +0000

    slider con propuesta de diseño

--- public/js/ingresar.js
+++ public/js/ingresar.js
@@ -1,3 +1,53 @@
+const $email = $('#email');
+const $password = $('#password');
+const $form = $('#form');
+const $ingresar = $('#ingresar');
+
+var cargarPagina = function(){
+	$form.submit(validateFields);
+	$ingresar.addClass('disabled');
+	emptyFields();
+	$email.keyup(emptyFields);
+	$password.keyup(emptyFields);
+	$ingresar.click(validateFields);
+}
+
+const emptyFields = ()=>{
+	if( $email.val().length == 0 || $password.val().length == 0 ){
+		$ingresar.addClass('disabled');
+	}else {
+		$ingresar.removeClass('disabled');
+	}
+}
+
+const validateEmail = ()=>{
+	let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+	if (emailRegex.test($email.val())) {
+		return true;
+	} else {
+		alert("Ingresa un email valido");
+	}
+};
+
+const validatePassword = ()=>{
+	if($password.val().length > 6){
+		alert("Recuerda que debes ingresar un password de 6 dígitos");
+	} else if ($password.val().length < 6){
+		alert("Recuerda que debes ingresar un password de mínimo 6 dígitos");
+	} else if($password.val().length == 6){ return true};
+}
+
+const validateFields = (e)=>{
+	e.preventDefault();
+	if(validateEmail() && validatePassword()){
+		userRegister();
+	} 
+}
+
+const userRegister = function(){
+	location.href="canal.html"
+};
+
 // Initialize Firebase
 var config = {
 	apiKey: "AIzaSyDhWXnUNzqUjE2BBr3wRfDV24rJMQDlLGU",
@@ -26,27 +76,65 @@ var loginWithGoogle = function(){
 };
 
 var login = function(provider){
-	
+
 	firebase.auth().signInWithPopup(provider)
 		.then(function(result) {
 		var token = result.credential.accessToken;
 		var user = result.user;
 		console.log(user);
-		var name = user.displayName;
-		var email = user.email;
-		localStorage.setItem('name', name);
+		var responseAPI = {
+			name : user.displayName,
+			email : user.email,
+			type: 2,
+			gender: 'Femenino',
+		}
+
+		localStorage.setItem('name', responseAPI.name);
+
+
+		loginUser(responseAPI);
 	})
-		.then(function(response) {
-			location.href = 'bienvenido.html';
+		.then(function() {
+
+		location.href = 'bienvenido.html';
 	})
 		.catch(function(error) {
 		var errorCode = error.code;
 		var errorMessage = error.message;
 		var email = error.email;
 		var credential = error.credential;
-		console.log('error', errorMessage);
 	});
+};
+
+
+var userRegisterApi = function(responseAPI){
+	$.post('http://www.imagentv.com/api/users/register', {
+		type: responseAPI.type,
+		email: responseAPI.email,
+		gender: responseAPI.gender,
+	})
+};
+
+var loginUser = function(responseAPI) {
+	console.log(responseAPI);
+	$.ajax({
+		url:'http://www.imagentv.com/api/users/login',
+		   type: 'POST',
+//		   dataType: 'jsonp',
+		   data: {
+		   type: responseAPI.type,
+		   email: responseAPI.email,
+		   }, 
+		   success: function(res){
+		console.log(res)
+	}, error: function(error){
+		console.log(error)
+	}
+	})
+
 }
+
+
 
 var fbButton = document.querySelector('.login-fb');
 var twitterButton = document.querySelector('.login-twitter');
@@ -55,3 +143,5 @@ var googleButton = document.querySelector('.login-google');
 fbButton.addEventListener('click', loginWithFb);
 twitterButton.addEventListener('click', loginWithTwitter);
 googleButton.addEventListener('click', loginWithGoogle);
+
+$(document).ready(cargarPagina);
